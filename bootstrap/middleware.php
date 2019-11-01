6ba672f0d49578a4a405a45052db02a620e1d54c
JR Cologne <kontakt@jr-cologne.de>
2019-11-02 00:07:04 +0100

    Set up basic error handling

--- bootstrap/middleware.php
+++ bootstrap/middleware.php
@@ -1,6 +1,27 @@
 <?php
 
+use Colognifornia\Web\Http\Controllers\Errors\HttpInternalServerErrorController;
+use Colognifornia\Web\Http\Controllers\Errors\HttpNotFoundController;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\Exception\HttpInternalServerErrorException;
+use Slim\Exception\HttpNotFoundException;
+use Slim\Psr7\Response;
 use Slim\Views\Twig;
 use Slim\Views\TwigMiddleware;
 
+$app->addRoutingMiddleware();
+
 $app->add(TwigMiddleware::createFromContainer($app, Twig::class));
+
+$errorMiddleware = $app->addErrorMiddleware(false, true, true);
+
+// Error Handlers
+$errorMiddleware->setErrorHandler(HttpNotFoundException::class, function (ServerRequestInterface $request) use ($container) {
+    return (new HttpNotFoundController($container->get(Twig::class)))
+        ->index($request, (new Response())->withStatus(404));
+});
+
+$errorMiddleware->setDefaultErrorHandler(function (ServerRequestInterface $request) use ($container) {
+    return (new HttpInternalServerErrorController($container->get(Twig::class)))
+        ->index($request, (new Response())->withStatus(500));
+});
