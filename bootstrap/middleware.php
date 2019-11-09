a98bbe93f46c4c189c55d702f035cb4b5d69e9fb
JR Cologne <kontakt@jr-cologne.de>
2019-11-09 19:18:53 +0100

    Add middleware to remove trailing slash from url

--- bootstrap/middleware.php
+++ bootstrap/middleware.php
@@ -2,8 +2,8 @@
 
 use Colognifornia\Web\Http\Controllers\Errors\HttpInternalServerErrorController;
 use Colognifornia\Web\Http\Controllers\Errors\HttpNotFoundController;
-use Psr\Http\Message\ServerRequestInterface;
-use Slim\Exception\HttpInternalServerErrorException;
+use Colognifornia\Web\Http\Middleware\RemoveTrailingSlashFromUrl;
+use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Exception\HttpNotFoundException;
 use Slim\Psr7\Response;
 use Slim\Views\Twig;
@@ -11,17 +11,19 @@ use Slim\Views\TwigMiddleware;
 
 $app->addRoutingMiddleware();
 
+$app->add(new RemoveTrailingSlashFromUrl());
+
 $app->add(TwigMiddleware::createFromContainer($app, Twig::class));
 
 $errorMiddleware = $app->addErrorMiddleware(false, true, true);
 
 // Error Handlers
-$errorMiddleware->setErrorHandler(HttpNotFoundException::class, function (ServerRequestInterface $request) use ($container) {
+$errorMiddleware->setErrorHandler(HttpNotFoundException::class, function (Request $request) use ($container) {
     return (new HttpNotFoundController($container->get(Twig::class)))
         ->index($request, (new Response())->withStatus(404));
 });
 
-$errorMiddleware->setDefaultErrorHandler(function (ServerRequestInterface $request) use ($container) {
+$errorMiddleware->setDefaultErrorHandler(function (Request $request) use ($container) {
     return (new HttpInternalServerErrorController($container->get(Twig::class)))
         ->index($request, (new Response())->withStatus(500));
 });
